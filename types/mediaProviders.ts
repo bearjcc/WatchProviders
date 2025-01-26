a79a3a1c5861335a8a1f5ff4ba81ad4fe79028da
Joseph C Caswell <bearjcc@users.noreply.github.com>
2025-01-26 17:44:36 -0500

    list of media providers

---
+++ types/mediaProviders.ts
@@ -0,0 +1,199 @@
+export interface MediaProvider {
+  name: string;
+  logo: string;
+  aliases: string[];
+}
+
+export const providers: MediaProvider[] = [
+  {
+    name: "Netflix",
+    logo: "/images/streaming_logos/Netflix.png",
+    aliases: ["Netflix basic with Ads", "Netflix Kids"]
+  },
+  {
+    name: "Amazon Prime Video",
+    logo: "/images/streaming_logos/Amazon Prime Video.png",
+    aliases: ["Amazon Prime Video with Ads", "Amazon Video"]
+  },
+  {
+    name: "HBO Max",
+    logo: "/images/streaming_logos/HBO Max.png",
+    aliases: ["Max", "HBO Now", "HBO Europe"]
+  },
+  {
+    name: "Hulu",
+    logo: "/images/streaming_logos/Hulu.png",
+    aliases: []
+  },
+  {
+    name: "Disney+",
+    logo: "/images/streaming_logos/Disney Plus.png",
+    aliases: ["Disney Plus"]
+  },
+  {
+    name: "Paramount+",
+    logo: "/images/streaming_logos/Paramount Plus.png",
+    aliases: [
+      "Paramount Plus",
+      "Paramount Plus Apple TV Channel",
+      "Paramount+ Amazon Channel",
+      "Paramount+ Roku Premium Channel",
+      "Paramount+ with Showtime"
+    ]
+  },
+  {
+    name: "U-Next",
+    logo: "/images/streaming_logos/U-Next.png",
+    aliases: []
+  },
+  {
+    name: "Apple TV+",
+    logo: "/images/streaming_logos/Apple TV Plus.png",
+    aliases: ["Apple TV Plus Amazon Channel"]
+  },
+  {
+    name: "Abema",
+    logo: "/images/streaming_logos/Abema.png",
+    aliases: []
+  },
+  {
+    name: "ESPN+",
+    logo: "/images/streaming_logos/ESPN+.png",
+    aliases: []
+  },
+  {
+    name: "Rakuten TV",
+    logo: "/images/streaming_logos/Rakuten TV.png",
+    aliases: []
+  },
+  {
+    name: "Discovery+",
+    logo: "/images/streaming_logos/Discovery+.png",
+    aliases: ["Discovery+ Amazon Channel"]
+  },
+  {
+    name: "FOD",
+    logo: "/images/streaming_logos/FOD.png",
+    aliases: []
+  },
+  {
+    name: "Joyn",
+    logo: "/images/streaming_logos/Joyn.png",
+    aliases: []
+  },
+  {
+    name: "RTL+",
+    logo: "/images/streaming_logos/RTL+.png",
+    aliases: []
+  },
+  {
+    name: "VIP",
+    logo: "/images/streaming_logos/VIP.png",
+    aliases: []
+  },
+  {
+    name: "Crunchyroll",
+    logo: "/images/streaming_logos/Crunchyroll.png",
+    aliases: ["Crunchyroll Amazon Channel"]
+  },
+  {
+    name: "DMM.com",
+    logo: "/images/streaming_logos/DMM.com.png",
+    aliases: []
+  },
+  {
+    name: "Peacock",
+    logo: "/images/streaming_logos/Peacock Premium.png",
+    aliases: ["Peacock Premium Plus"]
+  },
+  {
+    name: "Tubi",
+    logo: "/images/streaming_logos/Tubi TV.png",
+    aliases: []
+  },
+  {
+    name: "Lemino",
+    logo: "/images/streaming_logos/Lemino.png",
+    aliases: []
+  },
+  {
+    name: "Pluto TV",
+    logo: "/images/streaming_logos/Pluto TV.png",
+    aliases: []
+  },
+  {
+    name: "Roku Channel",
+    logo: "/images/streaming_logos/The Roku Channel.png",
+    aliases: []
+  },
+  {
+    name: "The CW",
+    logo: "/images/streaming_logos/The CW.png",
+    aliases: []
+  },
+  {
+    name: "Stan",
+    logo: "/images/streaming_logos/Stan.png",
+    aliases: []
+  },
+  {
+    name: "Canal+",
+    logo: "/images/streaming_logos/Canal+.png",
+    aliases: []
+  },
+  {
+    name: "Channel 4",
+    logo: "/images/streaming_logos/Channel 4.png",
+    aliases: ["Channel 4 Plus"]
+  },
+  {
+    name: "Crackle",
+    logo: "/images/streaming_logos/Crackle.png",
+    aliases: []
+  },
+  {
+    name: "Star+",
+    logo: "/images/streaming_logos/Star+.png",
+    aliases: []
+  },
+  {
+    name: "Telasa",
+    logo: "/images/streaming_logos/Telasa.png",
+    aliases: []
+  },
+  {
+    name: "Sky Showtime",
+    logo: "/images/streaming_logos/SkyShowtime.png",
+    aliases: []
+  },
+  {
+    name: "YouTube Movies",
+    logo: "/images/streaming_logos/YouTube.png",
+    aliases: []
+  },
+  {
+    name: "ITVX",
+    logo: "/images/streaming_logos/ITVX.png",
+    aliases: ["ITVX Premium"]
+  },
+  {
+    name: "NHK+",
+    logo: "/images/streaming_logos/NHK+.png",
+    aliases: []
+  },
+  {
+    name: "WOWOW",
+    logo: "/images/streaming_logos/WOWOW.png",
+    aliases: []
+  },
+  {
+    name: "TVer",
+    logo: "/images/streaming_logos/TVer.png",
+    aliases: []
+  },
+  {
+    name: "Unavailable",
+    logo: "/images/streaming_logos/Unavailable.png",
+    aliases: []
+  }
+];
