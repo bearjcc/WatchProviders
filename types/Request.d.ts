9e3f253e784b5dcd84a6e4f3430517a6106ff6fb
Joseph C Caswell <bearjcc@gmail.com>
2025-01-26 12:16:17 -0500

    feat: add more request types
    create a baserquest type for other request types to extend, add other request types

--- types/Request.d.ts
+++ types/Request.d.ts
@@ -2,52 +2,102 @@ import { ISODateTimeString } from "./ISODateTimeString.d.ts";
 import { Issue } from "./Issue.d.ts";
 import { OmbiUser } from "./User.d.ts";
 
-export interface MovieRequest {
-    theMovieDbId: number;
-    issueId: number;
-    issues?: Issue[];
-    subscribed: boolean;
-    showSubscribe: boolean;
-    is4kRequest: boolean;
-    rootPathOverride?: number;
-    qualityOverride?: number;
-    has4KRequest: boolean;
-    approved4K: boolean;
-    markedAsApproved4K?: TDateISOTimeStamp;
-    requestedDate4k?: TDateISOTimeStamp;
-    available4K: boolean;
-    markedAsAvailable4K?: TDateISOTimeStamp;
-    denied4K?: boolean;
-    markedAsDenied4K?: TDateISOTimeStamp;
-    deniedReason4K?: string;
-    requestCombination: number;
-    langCode?: string;
-    requestStatus?: string;
-    canApprove: boolean;
-    watchedByRequestedUser: boolean;
-    playedByUsersCount: number;
-    imdbId?: string;
-    overview?: string;
-    posterPath?: string;
-    releaseDate?: TDateISOTimeStamp;
-    digitalReleaseDate?: TDateISOTimeStamp;
-    status?: string;
-    background?: string;
-    released: boolean;
-    digitalRelease: boolean;
-    title?: string;
-    approved: boolean;
-    markedAsApproved?: TDateISOTimeStamp;
-    requestedDate: TDateISOTimeStamp;
-    available: boolean;
-    markedAsAvailable?: TDateISOTimeStamp;
-    requestedUserId?: string;
-    denied?: boolean;
-    markedAsDenied?: TDateISOTimeStamp;
-    deniedReason?: string;
-    requestType: number;
-    requestedByAlias?: string;
-    requestedUser?: OmbiUser;
-    source: number;
-    id: number;
-  }
+export interface BaseRequest {
+  id: number;
+  imdbId?: string;
+  title?: string;
+  overview?: string;
+  posterPath?: string;
+  releaseDate?: ISODateTimeString;
+  status?: string;
+  approved: boolean;
+  markedAsApproved?: ISODateTimeString;
+  requestedDate: ISODateTimeString;
+  available: boolean;
+  markedAsAvailable?: ISODateTimeString;
+  requestedUserId?: string;
+  denied?: boolean;
+  markedAsDenied?: ISODateTimeString;
+  deniedReason?: string;
+  requestType: number;
+  requestedByAlias?: string;
+  requestedUser?: OmbiUser;
+  source: number;
+  canApprove: boolean;
+  streamingProviders?: string[];
+}
+
+export interface MovieRequest extends BaseRequest {
+  theMovieDbId: number;
+  issueId: number;
+  issues?: Issue[];
+  subscribed: boolean;
+  showSubscribe: boolean;
+  is4kRequest: boolean;
+  rootPathOverride?: number;
+  qualityOverride?: number;
+  has4KRequest: boolean;
+  approved4K: boolean;
+  markedAsApproved4K?: ISODateTimeString;
+  requestedDate4k?: ISODateTimeString;
+  available4K: boolean;
+  markedAsAvailable4K?: ISODateTimeString;
+  denied4K?: boolean;
+  markedAsDenied4K?: ISODateTimeString;
+  deniedReason4K?: string;
+  requestCombination: number;
+  langCode?: string;
+  watchedByRequestedUser: boolean;
+  playedByUsersCount: number;
+  digitalReleaseDate?: ISODateTimeString;
+  background?: string;
+  released: boolean;
+  digitalRelease: boolean;
+}
+
+export interface TVRequest extends BaseRequest {
+  tvDbId: number;
+  externalProviderId: number;
+  qualityOverride: number;
+  rootFolder: number;
+  languageProfile: number;
+  totalSeasons: number;
+  childRequests: ChildRequest[];
+  background: string;
+}
+
+
+export interface ChildRequest extends BaseRequest {
+  parentRequest: string;
+  parentRequestId: number;
+  issueId: number;
+  seriesType: number;
+  subscribed: boolean;
+  showSubscribe: boolean;
+  releaseYear: ISODateTimeString;
+  issues: Issue[];
+  seasonRequests: SeasonRequest[];
+  requestStatus: string;
+  requestedUserPlayedProgress: number;
+}
+
+export interface SeasonRequest {
+  seasonNumber: number;
+  overview: string;
+  episodes: Episode[];
+  childRequestId: number;
+  childRequest: string;
+  seasonAvailable: boolean;
+  id: number;
+}
+
+export interface Episode extends BaseRequest {
+  episodeNumber: number;
+  airDate: ISODateTimeString;
+  url: string;
+  requested: boolean;
+  seasonId: number;
+  season: string;
+  airDateDisplay: string;
+  requestStatus: string;
+}
